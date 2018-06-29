ed622227663964dff7513b2a339298344cdb0034
liuchuangui <200537930@qq.com>
2018-06-29 09:41:29 +0800

    提交Service实现

---
+++ dubbo-api/src/main/java/com/cloudy/dubbo/demo/impl/DemoServiceImpl.java
@@ -0,0 +1,21 @@
+package com.cloudy.dubbo.demo.impl;
+
+import com.cloudy.dubbo.demo.DemoService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/6/29.
+ */
+public class DemoServiceImpl implements DemoService {
+
+    public List<String> getPermissions(Long id) {
+        List<String> demo = new ArrayList<String>();
+        demo.add(String.format("Permission_%d", id - 1));
+        demo.add(String.format("Permission_%d", id));
+        demo.add(String.format("Permission_%d", id + 1));
+
+        return demo;
+    }
+}
