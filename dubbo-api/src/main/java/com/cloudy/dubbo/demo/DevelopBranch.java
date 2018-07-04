b171e510d25ce7f06da2eeca487bd841cb9bd68e
liuchuangui <200537930@qq.com>
2018-07-04 16:47:37 +0800

    添加developBranch

--- dubbo-api/src/main/java/com/cloudy/dubbo/demo/DevelopBranch.java
+++ dubbo-api/src/main/java/com/cloudy/dubbo/demo/DevelopBranch.java
@@ -4,4 +4,7 @@ package com.cloudy.dubbo.demo;
  * Created by Administrator on 2018/7/4.
  */
 public class DevelopBranch {
+    public static void main(String[] args) {
+        System.out.println("develop branch");
+    }
 }
